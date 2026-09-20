1ad07676ddc10e056ae89a3b2e3e22cffc74a489
ibrahimtkmz <ibrahimtkmz@gmail.com>
2026-09-20 23:48:20 +0300

    fix: remove unsupported Next.js serverExternalPackages setting

--- next.config.js
+++ next.config.js
@@ -1,10 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  serverExternalPackages: [
-    "@remotion/vercel",
-    "@remotion/renderer",
-    "remotion",
-  ],
   outputFileTracingIncludes: {
     "/api/news/process": ["./remotion-build/**/*"],
   },
