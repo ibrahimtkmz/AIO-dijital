4f23266880b7d3be2ba239a04224989e6900aff3
ibrahimtkmz <ibrahimtkmz@gmail.com>
2026-09-20 14:29:13 +0300

    fix: include ffmpeg binary in server bundle

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  outputFileTracingIncludes: {
+    "/api/news/process": ["./node_modules/ffmpeg-static/ffmpeg"],
+  },
+};
 
 module.exports = nextConfig
