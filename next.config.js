fee0d4f577c92dfa68ce9dca74bcb82d48370dee
ibrahimtkmz <ibrahimtkmz@gmail.com>
2026-09-20 14:33:18 +0300

    fix: externalize ffmpeg-static for Vercel

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  serverExternalPackages: ["ffmpeg-static"],
   outputFileTracingIncludes: {
     "/api/news/process": ["./node_modules/ffmpeg-static/ffmpeg"],
   },
