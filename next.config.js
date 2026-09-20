6c0f2d09558f8e054baafa4eea985ba4c3a9a5e5
ibrahimtkmz <ibrahimtkmz@gmail.com>
2026-09-20 23:58:17 +0300

    fix: remove invalid Next.js tracing config

--- next.config.js
+++ next.config.js
@@ -1,8 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  outputFileTracingIncludes: {
-    "/api/news/process": ["./remotion-build/**/*"],
-  },
-};
+const nextConfig = {};
 
 module.exports = nextConfig;
