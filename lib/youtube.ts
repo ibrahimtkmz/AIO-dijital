17d0dbfeb35954423d7faba7e490abe1cccd44df
ibrahimtkmz <ibrahimtkmz@gmail.com>
2026-09-25 00:11:15 +0300

    refactor: limit OAuth scope to YouTube upload

--- lib/youtube.ts
+++ lib/youtube.ts
@@ -1,7 +1,4 @@
-const YOUTUBE_SCOPES = [
-  "https://www.googleapis.com/auth/youtube.upload",
-  "https://www.googleapis.com/auth/youtube.readonly",
-].join(" ");
+const YOUTUBE_SCOPES = "https://www.googleapis.com/auth/youtube.upload";
 const GOOGLE_AUTH_URL = "https://accounts.google.com/o/oauth2/v2/auth";
 const GOOGLE_TOKEN_URL = "https://oauth2.googleapis.com/token";
 const YOUTUBE_UPLOAD_URL = "https://www.googleapis.com/upload/youtube/v3/videos";
